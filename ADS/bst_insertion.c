813af9d7202d70fb66888c3cca9cd5c833bf177e
aruncpacp123 <aruncpacp10@gmail.com>
2023-11-17 19:34:37 +0530

    search

--- ADS/bst_insertion.c
+++ ADS/bst_insertion.c
@@ -12,12 +12,33 @@ struct node* create()
 }
 struct node* insert(struct node* n,struct node* r)
 {
-    if(r==NULL)
+    /*if(r==NULL)
         return n;
     if((n->data < r->data ))
         r=insert(n,r->left);
     else
-        r=insert(n,r->rigth);
+        r=insert(n,r->rigth);*/
+    if(root==NULL)
+    {
+        root=n;
+    }
+    else{
+        if((n->data < r->data )&& r->left!=NULL){
+            insert(n,r->left);
+        }
+        else if((n->data > r->data)&& r->rigth!=NULL){
+            insert(n,r->rigth);
+        }
+        else{
+            if((n->data < r->data ))
+            {
+                r->left=n;
+                return;
+            }
+            else
+                r->rigth=n;
+         }
+    }
 }
 void inorder(struct node *r)
 {
@@ -43,22 +64,31 @@ void postorder(struct node *r)
         inorder(r->rigth);
     printf("%d ",r->data);
 }
-int search(struct node *r,int n,int k)
+/*struct node* search(struct node *r,int n)
+{
+    if(r->data==n)
+        return r;
+    if(r->left!=NULL)
+        search(r->left,n);
+    if(r->rigth!=NULL)
+        search(r->rigth,n);
+}*/
+struct node* search(struct node *r,int n,struct node *r1)//in fn call search(root,n,NULL);
 {
     if(r->data==n)
     {
-        k=1;
-        return k;
+        r1=r;
+        return r1;
     }
     if(r->left!=NULL)
-        k=search(r->left,n,k);
-    if(r->rigth!=NULL)
-        k=search(r->rigth,n,k);
-    return k;
+        r1=search(r->left,n,r1);
+    else if(r->rigth!=NULL)
+        r1=search(r->rigth,n,r1);
 }
 void main()
 { 
-    int ch,op,a,k;
+    int ch,op,a;
+    struct node* k;
     dis:
     printf("\n 1.Insert \n 2.Delete \n 3.Traversal \n 4.Search \n 5.Exit");
     printf("\n Enter your choice :");
@@ -66,7 +96,7 @@ void main()
     switch(ch)
     {
         case 1:
-                printf("\n Enter the element to insert:");
+                /*printf("\n Enter the element to insert:");
                 scanf("%d",&a);
                 struct node *new=create();
                 new->data=a;
@@ -77,6 +107,14 @@ void main()
                 else
                     insert(new,root);
                 inorder(root);
+                break;*/
+                printf("\n Enter the element to insert:");
+                scanf("%d",&a);
+                struct node *new=create();
+                new->data=a;
+                new->left=NULL;
+                new->rigth=NULL;
+                insert(new,root);
                 break;
         case 3:
                 printf("\n 1.Inorder \n 2.Preorder \n 3.Postorder");
@@ -101,9 +139,9 @@ void main()
         case 4:
                 printf("\n Enter the node to search:");
                 scanf("%d",&a);
-                k=search(root,a,0);
-                if(k==1)
-                    printf("\n %d is presented",a);
+                k=search(root,a,NULL);
+                if(k!=NULL)
+                    printf("\n %d is presented",k->data);
                 else   
                     printf("\n %d is not presented",a);
                 break;
