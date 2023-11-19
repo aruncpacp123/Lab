361aabdeff967952c89c06996eaf53b5b54572bb
aruncpacp123 <aruncpacp10@gmail.com>
2023-11-19 17:35:47 +0530

    final

--- ADS/bit.c
+++ ADS/bit.c
@@ -154,8 +154,39 @@ void main()
         printf("%d ",cr[i]);
     
 }
-//second method to convert to bit string
- /*for(i=0,j=0;i<a;i++)
+/*
+/*#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+void main()
+{
+    int a,b,c,i,j;
+    void display(int a[],int n);
+    printf("\n Enter number of elements in the universal set:");
+    scanf("%d",&a);
+    int *n1=(int *)malloc(sizeof(int));
+    printf("\n Enter Universal Set:");
+    for(i=0;i<a;i++)
+        scanf("%d",&n1[i]);
+    char ar[a];//for bit string
+    char br[a];
+    char cr[a];
+    char dr[a];
+    char er[a];
+    printf("\n Enter number of elements in the first set:");
+    scanf("%d",&b);
+    printf("\n Enter the set1:");
+    int *n2=(int *)malloc(sizeof(int));//for original set
+    for(i=0;i<b;i++)
+        scanf("%d",&n2[i]);
+
+    printf("\n Enter number of elements in the Second set:");
+    scanf("%d",&c);
+    printf("\n Enter the set2:");
+    int *n3=(int *)malloc(sizeof(int));
+    for(i=0;i<c;i++)
+        scanf("%d",&n3[i]);
+    /*for(i=0,j=0;i<a;i++)
     {
         for(;j<b;)
         {
@@ -190,3 +221,84 @@ void main()
         }
     }
     */
+
+   /*
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            if(n1[i]==n2[j])
+            {
+                ar[i]=1;
+                break;
+            }
+        }
+        if(j==b)
+            ar[i]=0;
+    }
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            if(n1[i]==n3[j])
+            {
+                br[i]=1;
+                break;
+            }
+        }
+        if(j==c)
+            br[i]=0;
+    }
+    printf("\nUniversal set: ");
+    for(i=0;i<a;i++)
+        printf("%d ",n1[i]);
+    printf("\nSet1: ");
+    for(i=0;i<b;i++)
+        printf("%d ",n2[i]);
+    printf("\nSet1 using bit string: ");
+    for(i=0;i<a;i++)
+        printf("%d ",ar[i]);
+    printf("\nSet2: ");
+    for(i=0;i<c;i++)
+        printf("%d ",n3[i]);
+    printf("\nSet2 using bit string: ");
+    for(i=0;i<a;i++)
+        printf("%d ",br[i]);
+    for(i=0;i<a;i++)
+    {
+        cr[i]=ar[i]|br[i];//union OR
+    }
+    for(i=0;i<a;i++)
+    {
+        dr[i]=ar[i]&br[i];//intersection AND
+    }
+    for(i=0;i<a;i++)
+    {
+        if(ar[i]!=br[i]&& br[i]==0){
+            er[i]=1;//A-B
+        }
+        else
+            er[i]=0;
+    }
+    printf("\n Union:");
+    for(i=0;i<a;i++)
+        printf("%d ",cr[i]);
+    printf("\n Intersection: ");
+    for(i=0;i<a;i++)
+        printf("%d ",dr[i]);
+    printf("\n Union set:");
+    for(i=0;i<a;i++)
+        if(cr[i]==1)
+            printf("%d ",n1[i]);
+    printf("\n Intersection set:");
+    for(i=0;i<a;i++)
+        if(dr[i]==1)
+            printf("%d ",n1[i]);
+    printf("\n Set1-Set2:");
+    for(i=0;i<a;i++)
+        if(er[i]==1)
+            printf("%d ",n1[i]);
+    
+}*/
+
+
--- ADS/bit_string.c
+++ ADS/bit_string.c
@@ -1,35 +1,200 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdlib.h>
+int check(int a[],int n,int b)
+{
+    int i;
+    for(i=0;i<n;i++)
+        if(a[i]==b)
+            return 1;
+    if(i==n)
+        return 0;
+}
+void uni_read(int *a,int n)
+{
+    int d=0;
+    int c;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&c);
+        if(check(a,d,c))
+        {
+            i--;
+            printf("\n Element already exists!Reenter item:");
+        }
+        else
+        {
+            a[i]=c;
+            d++;
+        }
+    }
+}
+void read(int a[],int n,int *u,int n1)
+{
+    int d=0;
+    int c;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&c);
+        if(check(a,d,c))
+        {
+            i--;
+            printf("\n Element already exists!Reenter item:");
+        }
+        else if(!check(u,n1,c))
+        {
+            i--;
+            printf("\n Element Not in Universal set!reenter item:");
+        }
+        else
+        {
+            a[i]=c;
+            d++;
+        }
+    }
+}
+void print_bitarr(char ar[],int n)
+{
+    for(int i=0;i<n;i++)
+        printf("%d ",ar[i]);
+}
+void print_set(int *u,int a,char s1[])
+{
+    for(int i=0;i<a;i++)
+        if(s1[i]==1)
+            printf("%d ",u[i]);
+}
+void bit_str(int *u,int n1,int *s1,int n2,char r[])
+{
+    int i,j;
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+            if(u[i]==s1[j])
+            {
+                r[i]=1;
+                break;
+            }
+        }
+        if(j==n2)
+            r[i]=0;
+    }
+}
+void diff(char a[],int n,char b[],char r[])
+{
+    for(int i=0;i<n;i++)
+        if(a[i]!=b[i]&& b[i]==0)//A-B
+            r[i]=1;
+        else
+            r[i]=0;
+}
+void comp(int *u,int n,char a[],char r[])
+{
+    for(int i=0;i<n;i++)
+        if(u[i]!=a[i]&& a[i]==0)//A`
+            r[i]=1;
+        else
+            r[i]=0;
+}
 void main()
 {
     int a,b,c,i,j;
-    void display(int a[],int n);
+
     printf("\n Enter number of elements in the universal set:");
     scanf("%d",&a);
     int *n1=(int *)malloc(sizeof(int));
     printf("\n Enter Universal Set:");
-    for(i=0;i<a;i++)
-        scanf("%d",&n1[i]);
-    char ar[a];//for bit string
-    char br[a];
-    char cr[a];
-    char dr[a];
-    char er[a];
+    uni_read(n1,a);
+
+    char ar[a],br[a],cr[a];//for bit string
+
     printf("\n Enter number of elements in the first set:");
     scanf("%d",&b);
     printf("\n Enter the set1:");
     int *n2=(int *)malloc(sizeof(int));//for original set
-    for(i=0;i<b;i++)
-        scanf("%d",&n2[i]);
+    read(n2,b,n1,a);
 
     printf("\n Enter number of elements in the Second set:");
     scanf("%d",&c);
     printf("\n Enter the set2:");
     int *n3=(int *)malloc(sizeof(int));
-    for(i=0;i<c;i++)
-        scanf("%d",&n3[i]);
-    /*for(i=0,j=0;i<a;i++)
+    read(n3,c,n1,a);
+    //conveting to bit string
+    bit_str(n1,a,n2,b,ar);
+    bit_str(n1,a,n3,b,br);
+
+    
+
+    printf("\nUniversal set: ");
+    for(i=0;i<a;i++)
+        printf("%d ",n1[i]);
+
+    printf("\nSet1: ");
+    print_set(n1,a,ar);
+
+    printf("\nSet2: ");
+    print_set(n1,a,br);
+
+    printf("\nSet1 using bit string: ");
+    print_bitarr(ar,a);
+
+    printf("\nSet2 using bit string: ");
+    print_bitarr(br,a);
+
+    for(i=0;i<a;i++)
+        cr[i]=ar[i]|br[i];//union OR
+
+    printf("\n Union set:");
+    print_set(n1,a,cr);
+
+    printf("\n Union using bit string:");
+    print_bitarr(cr,a);
+
+    for(i=0;i<a;i++)
+        cr[i]=ar[i]&br[i];//intersection AND
+
+    printf("\n Intersection set:");
+    print_set(n1,a,cr);
+
+    printf("\n Intersection Using Bit String: ");
+    print_bitarr(cr,a);
+
+    diff(ar,a,br,cr);//A-B
+
+    printf("\n Set1-Set2:");
+    print_set(n1,a,cr);
+
+    printf("\n Set1-Set2 Using Bit String:");
+    print_bitarr(cr,a);
+
+    diff(br,a,ar,cr);//B-A
+    
+    printf("\n Set2-Set1:");
+    print_set(n1,a,cr);
+
+    printf("\n Set1-Set2 Using Bit String:");
+    print_bitarr(cr,a);
+
+    comp(n1,a,ar,cr);//A`
+    
+    printf("\n Set1 Compliment:");
+    print_set(n1,a,cr);
+
+    printf("\n Set1 Compliment Bit String:");
+    print_bitarr(cr,a);
+
+    comp(n1,a,br,cr);//B`
+    
+    printf("\n Set2 Compliment:");
+    print_set(n1,a,cr);
+
+    printf("\n Set2 Compliment Bit String:");
+    print_bitarr(cr,a);
+
+    
+}
+//second method to convert to bit string
+ /*for(i=0,j=0;i<a;i++)
     {
         for(;j<b;)
         {
@@ -64,80 +229,6 @@ void main()
         }
     }
     */
-    for(i=0;i<a;i++)
-    {
-        for(j=0;j<b;j++)
-        {
-            if(n1[i]==n2[j])
-            {
-                ar[i]=1;
-                break;
-            }
-        }
-        if(j==b)
-            ar[i]=0;
-    }
-    for(i=0;i<a;i++)
-    {
-        for(j=0;j<c;j++)
-        {
-            if(n1[i]==n3[j])
-            {
-                br[i]=1;
-                break;
-            }
-        }
-        if(j==c)
-            br[i]=0;
-    }
-    printf("\nUniversal set: ");
-    for(i=0;i<a;i++)
-        printf("%d ",n1[i]);
-    printf("\nSet1: ");
-    for(i=0;i<b;i++)
-        printf("%d ",n2[i]);
-    printf("\nSet1 using bit string: ");
-    for(i=0;i<a;i++)
-        printf("%d ",ar[i]);
-    printf("\nSet2: ");
-    for(i=0;i<c;i++)
-        printf("%d ",n3[i]);
-    printf("\nSet2 using bit string: ");
-    for(i=0;i<a;i++)
-        printf("%d ",br[i]);
-    for(i=0;i<a;i++)
-    {
-        cr[i]=ar[i]|br[i];//union OR
-    }
-    for(i=0;i<a;i++)
-    {
-        dr[i]=ar[i]&br[i];//intersection AND
-    }
-    for(i=0;i<a;i++)
-    {
-        if(ar[i]!=br[i]&& br[i]==0){
-            er[i]=1;//A-B
-        }
-        else
-            er[i]=0;
-    }
-    printf("\n Union:");
-    for(i=0;i<a;i++)
-        printf("%d ",cr[i]);
-    printf("\n Intersection: ");
-    for(i=0;i<a;i++)
-        printf("%d ",dr[i]);
-    printf("\n Union set:");
-    for(i=0;i<a;i++)
-        if(cr[i]==1)
-            printf("%d ",n1[i]);
-    printf("\n Intersection set:");
-    for(i=0;i<a;i++)
-        if(dr[i]==1)
-            printf("%d ",n1[i]);
-    printf("\n Set1-Set2:");
-    for(i=0;i<a;i++)
-        if(er[i]==1)
-            printf("%d ",n1[i]);
-    
-}
+
+
+
