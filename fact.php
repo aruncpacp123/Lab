37f8819f8155e54a43a1be64914ae4ada0361916
aruncpacp123 <aruncpacp10@gmail.com>
2023-11-22 13:45:13 +0530

    all

---
+++ fact.php
@@ -0,0 +1,23 @@
+<?php
+$n=$_POST['fact'];
+$fact=1;
+for($i=1;$i<$n;$i++)
+    $fact*=$i;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorail</title>
+</head>
+<body>
+    <form action="fact.php" method="post">
+        Enter a number:
+        <input type="text" value=<?=$n?> readonly>
+        <br><br><br>
+        Result:
+        <input type="text" value=<?=$fact?> readonly>
+    </form>
+</body>
+</html>
