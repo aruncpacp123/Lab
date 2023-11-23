8f8a9b3fc56b76a99508123dd7414ca2c6f1f5eb
aruncpacp123 <aruncpacp10@gmail.com>
2023-11-23 16:57:59 +0530

    pos

--- python_lab-main/positive.py
+++ python_lab-main/positive.py
@@ -1,7 +1,11 @@
-r=int(input("Enter a number :"))
-if r<0:
-    print(r," is a negative number")
-elif r>0:
-    print(r," is a positive number")
-else:
-    print(r," is zero")
+a=[]
+n=int(input("Enter number of elements in the list :"))
+print("Enter ",n," elements to list:")
+for i in range(0,n):
+    a.append(int(input()))
+print("list:",a)
+b=[]
+for x in a:
+    if x>0:
+        b.append(x)
+print("New list:",b)
